05deeb759202c468f5776d62639c544ae686e68d
nBryan <nbryan133@hotmail.com>
2025-07-09 09:29:28 -0400

    learned about making new pages, connecting to a database, working with JSON, and the postman application

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,15 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+
+    users = [
+      %{id: 1, name: "Jan", email: "jan@gmail.com"},
+      %{id: 2, name: "Greg", email: "greg@gmail.com"},
+      %{id: 3, name: "Bill", email: "bill@gmail.com"}
+    ]
+
+    render(conn, :users, users: users, layout: false)
+  end
 end
